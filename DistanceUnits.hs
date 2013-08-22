cae295887918e82a6d3bab7cf07cc505296f1748
Matt Gambogi <m@gambogi.com>
2013-08-21 23:48:20 -0400

    finished english system

--- DistanceUnits.hs
+++ DistanceUnits.hs
@@ -1,5 +1,10 @@
 module RunningDistance (
           Distance(..)
+        , Microinch
+        , Mil
+        , Line
+        , Barleycorn
+        , Inch
         , Foot
         , Yard
         , Mile
@@ -39,10 +44,6 @@ subDist x y = fromMeters (toMeters x - toMeters y)
 -- | Convert one distance to another.
 convertDist :: (Distance a, Distance b) => a -> b
 convertDist = fromMeters . toMeters
-
--- | Returns weather or not a distance qualifies as an ultra marathon
--- ultraMarathon :: Distance a => a -> Bool
-
 
 -- | Reads a string into a corresponding distance
 readDist :: (Float -> a) -> String ->
@@ -125,6 +126,21 @@ instance Read Barleycorn where
 
 --
 
+newtype Finger = Finger Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Inch where
+    toMeters    (Finger x)  = Meter ( x / 44.99)
+    fromMeters  (Meter x)   = Finger (x * 44.99)
+    toFloat     (Finger x)  = x
+
+instance Show Finger where
+    show (Finger x) = show x ++ "finger"
+instance Read Finger where 
+    readsPrec = readDist Finger "finger"
+
+--
+
 newtype Inch = Inch Float
     deriving (Eq, Ord, Num, Fractional)
 
@@ -140,6 +156,82 @@ instance Read Inch where
 
 --
 
+newtype Nail = Nail Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Nail where
+    toMeters    (Nail x)  = Meter (x / 17.5)
+    fromMeters  (Meter x) = Nail (x * 17.5)
+    toFloat     (Nail x)  = x
+
+instance Show Nail where
+    show (Nail x) = show x ++ "nail"
+instance Read Nail where
+    readsPrec = readDist Nail "nail"
+
+--
+
+newtype Palm = Palm Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Palm where
+    toMeters  (Palm x)   = Meter ( x / 13.12)
+    fromMeters (Meter x) = Palm  ( x * 13.12)
+    toFloat   (Palm x)   = x
+
+instance Show Palm where
+    show (Palm x) = show x ++ "palm"
+instance Read Palm where
+    readsPrec = readDist Palm "palm"
+
+--
+
+newtype Hand = Hand Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Hand where
+    toMeters (Hand x) = Meter ( x / 9.843)
+    fromMeters (Meter x) = Hand ( x * 9.843)
+    toFloat (Hand x)    = x
+
+instance Show Hand where
+    show (Hand x) = show x ++ "hand"
+
+instance Read Hand where
+    readsPrec = readDist Hand "hand"
+
+--
+
+newtype Link = Link Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Link where 
+    toMeters (Link x) = Meter ( x / 4.971)
+    fromMeters (Meter x) = Link ( x * 4.971)
+    toFloat (Link x) = x
+
+instance Show Link where
+    show (Link x) = x ++ "link"
+
+instance Read Link where
+    readsPrec = readDist Link "link"
+
+--
+
+newtype Span = Span Float 
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Span where
+    toMeters (Span x) = Meter ( x / 4.374)
+
+
+instance Show Span where
+    show (Span x) = show x ++ "span"
+instance Read Span where
+    readsPrec = readDist Span "span"
+
+--
+
 newtype Foot = Foot Float
     deriving (Eq, Ord, Num, Fractional)
 
@@ -152,6 +244,22 @@ instance Show Foot where
 
 instance Read Foot where
     readsPrec = readDist Foot "ft"
+
+--
+
+newtype Cubit = Cubit Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Cubit where
+    toMeters (Cubit x) = Meter ( x / 2.187)
+    fromMeters (Meter x) = Cubit (x * 2.187)
+    toFloat (Cubit x) = x
+
+instance Show Cubit where
+    show (Cubit x) = x ++ "cubit"
+instance Read Cubit where
+    readsPrec = readDist Cubit "cubit"
+
 --
 
 newtype Yard = Yard Float
@@ -168,6 +276,99 @@ instance Read Yard where
     readsPrec = readDist Yard "yd"
 
 --
+
+newtype Ell = Ell Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Ell where
+    toMeters (Ell x) = Meter ( x / 0.8749)
+    fromMeters (Meter x) = Ell ( x* 0.8749)
+    toFloat (Ell x) = x
+   
+instance Show Ell where
+    show (Ell x) = show x ++ "ell"
+
+instance Read Ell where
+    readsPrec = readDist Ell "ell"
+
+--
+
+newtype Fathom = Fathom Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Fathom where
+    toMeters (Fathom x) = Meter (x / 0.5468)
+    fromMeters (Meter x) = Fathom ( x * 0.5468)
+    toFloat (Fathom x) = x
+
+instance Show Fathom where
+    show (Fathom x) = show x ++ "fathom"
+instance Read Fathom where
+    readsPrec = readDist Fathom "fathom"
+
+--
+
+newtype Perch = Perch Float
+    deriving (Eq, Ord, Num, Fractional)
+    
+instance Distance Perch where
+    toMeters (Perch x) = Meter ( x / 0.1988)
+    fromMeters (Meter x) = Perch ( x * 0.1988)
+    toFloat (Perch x) = x
+
+instance Show Perch where
+    show (Perch x) = show x ++ "perch"
+
+instance Read Perch where
+    readsPrec = readDist Perch "perch"
+
+--
+
+newtype Rod = Rod Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Rod where
+    toMeters (Rod x) = Meter ( x / 0.1988)
+    fromMeter (Meter x) = Rod ( x * 0.1988)
+    toFloat (Rod x) = x
+
+instance Show Rod where
+    show (Rod x) = show x ++ "rd"
+instance Read Rod where
+    readsPrec = readDist Rod "rd"
+
+--    
+
+newtype Chain = Chain Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Chain where
+    toMeters (Chain x) = Meter ( x / 0.04971)
+    fromMeters (Meter x) = Chain ( x * 0.04971)
+    toFloat (Chain x) = x
+
+instance Show Chain where
+    show (Chain x) = show x ++ "chain"
+instance Read Chain where
+    readsPrec = readDist Chain "chain"
+
+--
+
+newtype Furlong = Furlong Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance Chain where
+    toMeters (Furlong x) = Meter ( x / 0.004971)
+    fromMeters (Meter x) = Furlong ( x * 0.004971)
+    toFloat (Furlong x) = x
+
+instance Show Furlong where
+    show (Furlong x) = x ++ "furlong"
+instance Read Furlong where
+    readsPrec = readDist Furlong "furlong"
+
+--
+
 newtype Mile = Mile Float
     deriving (Eq, Ord, Num, Fractional)
 
@@ -180,7 +381,24 @@ instance Show Mile where
 
 instance Read Mile where
     readsPrec = readDist Mile "mi"
+
 --
+
+newtype League = League Float
+    deriving (Eq, Ord, Num, Fractional)
+
+instance Distance League where
+    toMeters (League x) = Meter (x / 0.0002071)
+    fromMeters ( Meter x) = League ( x * 0.0002071)
+    toFloat (League x) = x
+
+instance Show League where 
+    show (League x)  = show x ++ "league"
+instance Read League where
+    readsPrec = readDist League "league"
+
+--
+
 
 -- | Metric Ascending
 
